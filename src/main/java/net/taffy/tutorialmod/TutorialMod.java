39a956356c1e5a116312c8f707eee91652f1f173
TaffyTanuki <rainbowghost1708@gmail.com>
2024-07-26 01:20:57 +0200

    idk

---
+++ src/main/java/net/taffy/tutorialmod/TutorialMod.java
@@ -0,0 +1,23 @@
+package net.taffy.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.taffy.tutorialmod.block.ModBlocks;
+import net.taffy.tutorialmod.item.ModItemGroups;
+import net.taffy.tutorialmod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+
+	}
+}
